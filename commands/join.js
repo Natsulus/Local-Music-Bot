aa5b0c00d678bad4050dd2c6debaefae91a40f1c
Aetylus <Aetylus@users.noreply.github.com>
2016-07-17 03:21:00 -0400

    Added Dockerfile for use with Docker and updated the join command to only look at voice channels. This fixes an issue where a text channel may have a similar name to the voice channel and the bot fail to join the correct one.

--- commands/join.js
+++ commands/join.js
@@ -10,7 +10,7 @@ exports.execute = function (data, cm) {
         if (!cm.bot.voiceConnection) {
             const check = new RegExp(`.*${data.args}.*`, 'i');
 
-            for (const channel of data.m.channel.server.channels) {
+            for (const channel of data.m.channel.server.channels.getAll('type', 'voice')) {
                 if (channel.name.search(check) !== -1) {
                     cm.bot.joinVoiceChannel(channel);
                     break;
